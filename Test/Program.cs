b8eb30288db65b003c66679ecd2655d9c8ae897b
mariaulybina <59221683+mariaulybina@users.noreply.github.com>
2020-02-01 14:15:53 +0300

    New

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,7 +6,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, GitHub");
+            Console.WriteLine("Hello, GitHub.");
             Console.WriteLine("Проверка изменений на сервере");
         }
     }
