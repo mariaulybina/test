60086fa5ca05c2707b74f1870275d7793b6fa399
Мария Улыбина <maryulybina@gmail.com>
2020-02-01 14:17:24 +0300

    Revert "Revert "New""
    This reverts commit 6394632fbf23d910538b3eda579e759df4825d9a.

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,6 +6,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, GitHub");
             Console.WriteLine("Проверка изменений на сервере");
         }
     }
